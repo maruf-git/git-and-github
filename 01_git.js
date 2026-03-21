dfd924023d9c121564eb053520eaa51b226fc822
maruf-git <munna.csehstu@gmail.com>
2026-03-21 22:14:09 +0600

    introduction to git

---
+++ 01_git.js
@@ -0,0 +1,39 @@
+// Where to use Git?
+// Git works on your computer, but you also use it with online services like GitHub, GitLab, or Bitbucket to share your work with others. These are called remote repositories.
+
+// What Changes When You Switch Platform?
+// The tutorial can show examples for GitHub, GitLab, or Bitbucket.
+
+// The Git commands are mostly the same, but some steps (like connecting to a remote or pushing code) might look a little different depending on the platform you choose
+
+// What is Git?
+// Git is a popular version control system.
+
+// It was created by Linus Torvalds in 2005, and has been maintained by Junio Hamano since then.
+
+// It is used for:
+
+// Tracking code changes
+// Tracking who made changes
+// Coding collaboration
+
+
+// Key Git Concepts
+// Repository: A folder where Git tracks your project and its history.
+// Clone: Make a copy of a remote repository on your computer.
+// Stage: Tell Git which changes you want to save next.
+// Commit: Save a snapshot of your staged changes.
+// Branch: Work on different versions or features at the same time.
+// Merge: Combine changes from different branches.
+// Pull: Get the latest changes from a remote repository.
+// Push: Send your changes to a remote repository.
+
+// Working with Git
+// Initialize Git on a folder, making it a Repository
+// Git now creates a hidden folder to keep track of changes in that folder
+// When a file is changed, added or deleted, it is considered modified
+// You select the modified files you want to Stage
+// The Staged files are Committed, which prompts Git to store a permanent snapshot of the files
+// Git allows you to see the full history of every commit.
+// You can revert back to any previous commit.
+// Git does not store a separate copy of every file in every commit, but keeps track of changes made in each commit!
