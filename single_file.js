96c296338014c4c61ba6d616d31b326df5e882c4
maruf-git <munna.csehstu@gmail.com>
2026-03-23 23:47:22 +0600

    added multiple_files using git add --all command

--- single_file.js
+++ single_file.js
@@ -1,3 +1,8 @@
 // we will add(stage) this single file using
 // git add file_name syntax
-// so it will be git add single_file.js     
+// so it will be git add single_file.js
+// then we will commit stagd a single file and pushed it to
+// remote
+
+// for unstaging
+// git restore --staged filename
